e5a5cbb5f9e05d464a8f979831690f91d740cafe
chulqyfauzan <chulqy.teave@gmail.com>
2018-02-21 11:33:07 +0700

    versi 0.1

---
+++ tambahtambah.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B, C;
+	cout<<"program tambah tambahan \n";
+	cout<<"masukkan angka pertama : "; cin>>A;
+	cout<<"masukkan angka kedua   : "; cin>>B;
+	C = A+B;
+	cout<<"hasilnya adalah        : "; cout<<C;
+	return 0;
+}
