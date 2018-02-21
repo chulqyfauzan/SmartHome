4448af467c3f22fb5cc7000a961cd526ea73ed5e
chulqyfauzan <chulqy.teave@gmail.com>
2018-02-21 11:42:59 +0700

    versi 0.2

--- tambahtambah.cpp
+++ tambahtambah.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	int A, B, C;
-	cout<<"program tambah tambahan \n";
+	cout<<"program tambah tambahan versi 1 \n";
 	cout<<"masukkan angka pertama : "; cin>>A;
 	cout<<"masukkan angka kedua   : "; cin>>B;
 	C = A+B;
